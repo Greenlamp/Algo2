3476a5a91d864e4f2592089ec2470624501201d9
gknuts <gknuts@ulb.ac.be>
2013-12-17 14:17:00 +0100

    Modification de Main.
    Construction du graphe sur base du fichier exemple.

--- Projet_Algo2/src/core/Main.java
+++ Projet_Algo2/src/core/Main.java
@@ -1,7 +1,24 @@
 package core;
 
+import graph.Graph;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
     public static void main(String[] args){
         String nameFile = args[0];
+        
+        Graph graph = new Graph();
+        try {
+            graph.load(nameFile);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
