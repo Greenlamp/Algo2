63cfac3625d0e2dcf4eeb599683c2ead055b79d1
gknuts <gknuts@ulb.ac.be>
2013-12-17 14:12:52 +0100

    Ajout de la classe Graph.
    Classe un graphe.

---
+++ Projet_Algo2/src/graph/Graph.java
@@ -0,0 +1,196 @@
+package graph;
+
+import graphviz.GraphViz;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class Graph {
+    private Node firstNode;
+    public Graph(){
+        firstNode = null;
+    }
+
+    public void addNode(String id, String name, int value) {
+        Node newNode = new Node();
+        newNode.setName(name);
+        newNode.setValue(value);
+        newNode.setId(id);
+        if(this.getFirstNode() == null){
+            this.setFirstNode(newNode);
+        }else{
+            Node node = this.getFirstNode();
+            while(node.getNextNode() != null){
+                node = node.getNextNode();
+            }
+            newNode.setPrevNode(node);
+            node.setNextNode(newNode);
+        }
+    }
+
+    public void addArc(String nameSrc, String nameDest, int value) throws Exception {
+        Node node = this.getFirstNode();
+        Node nodeSrc = null;
+        Node nodeDest = null;
+        while(node != null || (nodeSrc == null && nodeDest == null)){
+            if(nodeSrc == null && node.getName().equals(nameSrc)){
+                nodeSrc = node;
+            }else if(node.getName().equals(nameDest)){
+                nodeDest = node;
+            }
+            node = node.getNextNode();
+        }
+        if(nodeSrc != null && nodeDest != null){
+            Arc newArc = new Arc();
+            newArc.setOriginNode(nodeSrc);
+            newArc.setDestNode(nodeDest);
+            newArc.setLabel(String.valueOf(value));
+            Arc arcSrc = nodeSrc.getArc();
+            if(arcSrc == null){
+                nodeSrc.setArc(newArc);
+            }else{
+                while(arcSrc.getNextArc() != null){
+                    arcSrc = arcSrc.getNextArc();
+                }
+                arcSrc.setNextArc(newArc);
+            }
+        }else{
+            if(nodeSrc == null && nodeDest == null){
+                throw new Exception("Node source et node destination introuvable");
+            }else if(nodeSrc == null){
+                throw new Exception("Node source introuvable");
+            }else if(nodeDest == null){
+                throw new Exception("Node destination introuvable");
+            }
+        }
+    }
+
+    public void toPng(String nameFile) {
+        GraphViz gv = new GraphViz();
+        gv.addln(gv.start_digraph());
+        gv.addln("node [color=orange, style=filled]");
+        Node node = this.getFirstNode();
+        while(node != null){
+            Arc arc = node.getArc();
+            while(arc != null){
+                gv.addln(arc.getOriginNode().getId() + " -> " + arc.getDestNode().getId() + "[label="+arc.getLabel()+"]");
+                arc = arc.getNextArc();
+            }
+            node = node.getNextNode();
+        }
+        node = this.getFirstNode();
+        while(node != null){
+            gv.addln(node.getId() + " [label = \""+node.getName()+"\n"+node.getValue()+"\"]");
+            node = node.getNextNode();
+        }
+        gv.addln(gv.end_graph());
+        System.out.println(gv.getDotSource());
+        String type = "png";
+        File out = new File(nameFile + "." + type);
+        byte[] graph = gv.getGraph(gv.getDotSource(), type);
+        gv.writeGraphToFile(gv.getGraph(gv.getDotSource(), type), out );
+    }
+
+    public void load(String nameFile) throws FileNotFoundException, IOException, Exception {
+        LinkedList<String> data = new LinkedList<>();
+        FileInputStream fis = new FileInputStream(nameFile);
+        DataInputStream dis = new DataInputStream(fis);
+        BufferedReader br = new BufferedReader(new InputStreamReader(dis));
+        String line = null;
+        while((line = br.readLine()) != null){
+            data.add(line);
+        }
+        br.close();
+        
+        if(data.size() == 0){
+            throw new Exception("Fichier vide");
+        }
+        
+        int nbSommet = Integer.parseInt(data.removeFirst());
+        for(int i=0; i<nbSommet; i++){
+            String[] sommet = data.get(i).split(" ");
+            String nameSommet = sommet[0];
+            int valueSommet = Integer.parseInt(sommet[1]);
+            this.addNode(String.valueOf(i), nameSommet, valueSommet);
+        }
+        for(int i=0; i<nbSommet; i++){
+            data.removeFirst();
+        }
+        for(String arc : data){
+            String[] split = arc.split(" ");
+            String nameSrc = split[0];
+            String nameDest = split[1];
+            int value = Integer.parseInt(split[2]);
+            try {
+                this.addArc(nameSrc, nameDest, value);
+            } catch (Exception ex) {
+                throw new Exception("Fichier incorrect");
+            }
+        }
+        data.clear();
+    }
+
+    public void pronfondeur(Node node) throws Exception {
+        node.setMarque(true);
+        System.out.println(node.getName());
+        Arc arc = node.getArc();
+        while(arc != null){
+            if(arc.getDestNode() != null && !arc.getDestNode().isMarque()){
+                pronfondeur(arc.getDestNode());
+            }
+            arc = arc.getNextArc();
+        }
+    }
+
+    public void largeur() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void showCycles() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int degre(String nom) throws Exception {
+        int cpt = 0;
+        boolean found = false;
+        Node node = this.getFirstNode();
+        while(node != null && !found){
+            if(node.getName().equals(nom)){
+                found = true;
+                Arc arc = node.getArc();
+                while(arc != null){
+                    cpt++;
+                    arc = arc.getNextArc();
+                }
+            }
+            node = node.getNextNode();
+        }
+        if(!found){
+            throw new Exception("Sommet introuvable");
+        }
+        return cpt;
+    }
+
+    public Node getFirstNode() {
+        return firstNode;
+    }
+
+    public void setFirstNode(Node firstNode) {
+        this.firstNode = firstNode;
+    }
+
+    public void unmark() {
+        Node node = this.firstNode;
+        while(node != null){
+            node.setMarque(false);
+            node = node.getNextNode();
+        }
+    }
+    
+    
+}
