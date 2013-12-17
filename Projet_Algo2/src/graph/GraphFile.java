e881604060c40608173f2c6ff6cd4b2272d469e6
gknuts <gknuts@ulb.ac.be>
2013-12-17 15:03:56 +0100

    Ajout de la classe GraphFile.
    Classe permettant de charger et sauvegarde un graphe dans un fichier.

---
+++ Projet_Algo2/src/graph/GraphFile.java
@@ -0,0 +1,54 @@
+package graph;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class GraphFile {
+
+    public static Graph load(String nameFile) throws FileNotFoundException, IOException, Exception{
+        Graph graph = new Graph();
+        LinkedList<String> data = new LinkedList<>();
+        FileInputStream fis = new FileInputStream(nameFile);
+        DataInputStream dis = new DataInputStream(fis);
+        BufferedReader br = new BufferedReader(new InputStreamReader(dis));
+        String line = null;
+        while((line = br.readLine()) != null){
+            data.add(line);
+        }
+        br.close();
+        
+        if(data.size() == 0){
+            throw new Exception("Fichier vide");
+        }
+        
+        int nbSommet = Integer.parseInt(data.removeFirst());
+        for(int i=0; i<nbSommet; i++){
+            String[] sommet = data.get(i).split(" ");
+            String nameSommet = sommet[0];
+            int valueSommet = Integer.parseInt(sommet[1]);
+            graph.addNode(String.valueOf(i), nameSommet, valueSommet);
+        }
+        for(int i=0; i<nbSommet; i++){
+            data.removeFirst();
+        }
+        for(String arc : data){
+            String[] split = arc.split(" ");
+            String nameSrc = split[0];
+            String nameDest = split[1];
+            int value = Integer.parseInt(split[2]);
+            try {
+                graph.addArc(nameSrc, nameDest, value);
+            } catch (Exception ex) {
+                throw new Exception("Fichier incorrect");
+            }
+        }
+        data.clear();
+        return graph;
+    }
+
+}
